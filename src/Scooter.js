c55537835932ff5588f545d4dd96a3cda2be26e5
Giulia Champness <Giulia.Champness@hl.co.uk>
2023-02-10 14:35:35 +0000

    added scooter test and logic

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,35 @@
-class Scooter{
-  // scooter code here
+class Scooter {
+  static nextSerial = 1;
+
+  constructor(station) {
+    this.station = station || null;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 0;
+    this.isBroken = false;
+  }
+
+  checkOut(user) {
+    this.user = user;
+    this.station = null;
+  }
+
+  checkIn(station) {
+    this.user = null;
+    this.station = station;
+  }
+
+  reportBroken() {
+    this.isBroken = true;
+  }
+
+  repair() {
+    this.isBroken = false;
+  }
+
+  chargeScooter(amount) {
+    this.charge = Math.min(100, this.charge + amount);
+  }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,72 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
+describe("scooter integrity checks", () => {
+  const scooter = new Scooter("Bristol");
+  test("has correct properties", () => {
     // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+    expect(scooter).toHaveProperty("user", null);
+    expect(scooter).toHaveProperty("serial");
+    expect(scooter).toHaveProperty("charge");
+    expect(typeof scooter.isBroken).toBe("boolean");
+    expect(scooter.station).toBe("Bristol");
+  });
+});
 
 //Method tests
-describe('scooter methods', () => {
-  // tests here!
+describe("scooter methods", () => {
+  let scooter;
+  let user;
 
-  //rent method
+  beforeEach(() => {
+    scooter = new Scooter("Bristol");
+    user = new User();
+  });
 
-  //dock method
+  it("should allow a scooter to be checked out by a user", () => {
+    scooter.checkOut(user);
+    expect(scooter.user).toBe(user);
+    expect(scooter.station).toBe(null);
+  });
 
-  //requestRepair method
+  it("should allow a scooter to be checked in to a station", () => {
+    scooter.checkOut(user);
+    scooter.checkIn("Bristol");
+    expect(scooter.user).toBe(null);
+    expect(scooter.station).toBe("Bristol");
+  });
 
-  //charge method
+  it("should allow a scooter to be reported as broken", () => {
+    scooter.reportBroken();
+    expect(scooter.isBroken).toBe(true);
+  });
 
-})
+  it("should allow a scooter to be repaired", () => {
+    scooter.reportBroken();
+    scooter.repair();
+    expect(scooter.isBroken).toBe(false);
+  });
+
+  it("should allow a scooter to be charged", () => {
+    scooter.chargeScooter(50);
+    expect(scooter.charge).toBe(50);
+  });
+
+  it("should limit the charge to 100", () => {
+    scooter.chargeScooter(150);
+    expect(scooter.charge).toBe(100);
+  });
+});
+
+//describe('scooter methods', () => {
+// tests here!
+
+//rent method
+
+//dock method
+
+//requestRepair method
+
+//charge method
+//}
