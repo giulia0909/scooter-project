eafc8c745ade3df1d252e044126b741cc55fd866
Giulia Champness <Giulia.Champness@hl.co.uk>
2023-02-13 10:26:27 +0000

    added user test and logic

--- src/User.js
+++ src/User.js
@@ -5,6 +5,46 @@ class User {
     this.age = age;
     this.loggedIn = loggedIn;
   }
+  //_username is a private property used to store value of username
+  //getter returns value of _username
+  get username() {
+    return this._username;
+  }
+  //setter updates _username
+  set username(value) {
+    this._username = value;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(value) {
+    if (value < 0) {
+      throw new Error("Age cannot be set to a negative value.");
+    }
+    this._age = value;
+  }
+
+  get loggedIn() {
+    return this._loggedIn;
+  }
+
+  set loggedIn(value) {
+    this._loggedIn = value;
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
 }
 
 module.exports = User;
--- tests/User.test.js
+++ tests/User.test.js
@@ -7,21 +7,77 @@ const User = require("../src/User");
 
 // User tests here
 describe("user integrity checks", () => {
-  const user = new User("General Grievous", "password123", 30, true);
+  const user = new User("General Grievous", "password123", 65, true);
   test("has correct properties", () => {
     expect(user).toHaveProperty("username", "General Grievous");
     expect(user).toHaveProperty("password", "password123");
-    expect(user).toHaveProperty("age", 30);
+    expect(user).toHaveProperty("age", 65);
     expect(user).toHaveProperty("loggedIn", true);
   });
 });
 
 // test username
+describe("Username", () => {
+  let user;
+  beforeEach(() => {
+    user = new User("Luke", "skywalker", 25, false);
+  });
 
-// test password
+  test("Can be read", () => {
+    expect(user.username).toBe("Luke");
+  });
 
-// test age
+  test("Cannot be set", () => {
+    const setUsername = () => {
+      user.username = "Anakin";
+    };
+    expect(setUsername).toThrow();
+    expect(user.username).toBe("Luke");
+  });
+});
 
-// test login
+// test method password
+describe("User password", () => {
+  const user = new User("Luke Skywalker", "jedi", 20, false);
 
-// test logout
+  test("password can be set", () => {
+    user.password = "theForce";
+    expect(user.password).toBe("theForce");
+  });
+
+  test("password must be at least 6 characters long", () => {
+    user.password = "short";
+    expect(user.password).toBe("theForce");
+  });
+});
+
+// test method age - the first test case uses toBe to verify that the age property of the user object has the expected value of 50.
+//The second test case sets the age property to a negative value and uses toBe to verify that the age property has not changed.
+describe("User age", () => {
+  const user = new User("Mace Windu", "jedi", 50, true);
+  test("has correct age", () => {
+    expect(user.age).toBe(50);
+  });
+  test("cannot be set to a negative value", () => {
+    user.age = -30;
+    expect(user.age).toBe(50);
+  });
+});
+
+// test method login
+describe("User.login", () => {
+  const user = new User("Anakin Skywalker", "padawan", 25, false);
+  test("logs in the user", () => {
+    user.login();
+    expect(user.loggedIn).toBe(true);
+  });
+});
+
+// test method logged out
+describe("User.logout", () => {
+  const user = new User("Yoda", "jediMaster", 900, true);
+  test("logs out the user", () => {
+    user.logout();
+    expect(user.loggedIn).toBe(false);
+  });
+});
